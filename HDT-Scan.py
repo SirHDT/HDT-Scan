c56af872e59fb94fe8118e74a75134872c9ac393
SirHDT <165179711+SirHDT@users.noreply.github.com>
2024-03-28 11:23:20 +0000

    Update HDT-Scan.py 1.2
    Add Troubleshooting
    Add Wordlist path check if exists

--- HDT-Scan.py
+++ HDT-Scan.py
@@ -1,4 +1,5 @@
 import subprocess
+import os
 
 def nmap_scan(target, ports):
     try:
@@ -9,6 +10,9 @@ def nmap_scan(target, ports):
         print(result.stdout)
     except FileNotFoundError:
         print("Nmap not found. Please install Nmap on your system.")
+    except subprocess.CalledProcessError as error:
+        print(f"Error: Nmap command failed with return code {error.returncode}.")
+        print("Output:", error.output)
 
 def rustscan(target):
     try:
@@ -44,17 +48,46 @@ def gobuster(target, port, wordlist):
     except FileNotFoundError:
         print("Gobuster not found. Please install Gobuster on your system.")
 
+def get_valid_input(prompt):
+    while True:
+        user_input = input(prompt).strip().lower()
+        if user_input == 'y' or user_input == 'n':
+            return user_input
+        else:
+            print("Invalid input. Please enter 'Y' or 'N'.")
+
+def is_valid_path(path):
+    # Check if the given path exists.
+    return os.path.exists(path)
+
+def get_wordlist_path():
+    while True:
+        wordlist_path = input("Enter custom wordlist path: ").strip()
+        if os.path.exists(wordlist_path):
+            print("Wordlist path is valid.")
+            return wordlist_path
+        else:
+            print("Error: Wordlist path does not exist.")
+
 def main():
-    print("HDT Scan!")
-    target = input("Target IP: ")
-    rustscan(target)
-    skip_nmap = input("Do you want to skip Nmap scan and proceed directly to Gobuster? (Y/N): ").lower() == 'y'
-    if not skip_nmap:
-        ports = input("Ports: ")
-        nmap_scan(target, ports)
-    port = input("Web Port: ")
-    wordlist = input("Enter custom wordlist path: ")
-    gobuster(target, port, wordlist)
+    try:
+        print("HDT Scan!")
+        target = input("Target IP: ")
+        rustscan(target)
+        skip_nmap = get_valid_input("Do you want to skip Nmap scan and proceed directly to Gobuster? (Y/N): ")
+        if not skip_nmap:
+            ports = input("Ports: ")
+            nmap_scan(target, ports)
+        port = input("Web Port: ")
+        wordlist = get_wordlist_path()
+        print("Wordlist path:", wordlist)
+        gobuster(target, port, wordlist)
+    except KeyboardInterrupt:
+        print("\nProgram terminated by user.")
+        exit(1)
+    except Exception as e:
+        print("An unexpected error occurred:", e)
+        exit(1)
 
 if __name__ == "__main__":
     main()
